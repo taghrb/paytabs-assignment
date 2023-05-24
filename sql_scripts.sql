c0059a3b1d6ca88f1ce67f9965a1bdff047ac5c5
taghrb <taghrb.developer@gmail.com>
2023-05-24 13:54:31 +0300

    - database script added for sample data insertion

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -7,3 +7,13 @@ CREATE TABLE categories (
   parent_id INT,
   category_name VARCHAR(255)
 );
+
+INSERT INTO `categories` (`category_id`, `parent_id`, `category_name`) VALUES
+(1, NULL, 'Category A'),
+(2, NULL, 'Category B'),
+(3, 2, 'Sub B1'),
+(4, 2, 'Sub B2'),
+(5, 4, 'SUB SUB B2-1'),
+(6, 4, 'SUB SUB B2-2'),
+(7, 5, 'SUB SUB SUB B2-1-1'),
+(8, 5, 'SUB SUB SUB B2-1-2');
